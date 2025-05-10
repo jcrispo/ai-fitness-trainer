cf4aacfe4185e9e70cafe2a4a8a7e4380f8d66a3
Jason Ray <jason.crispo@gmail.com>
2025-05-10 16:32:13 +0800

    set up Springboot, add Actuatior config

---
+++ backend/services/auth-service/src/main/java/io/github/jcrispo/auth/AuthServiceApplication.java
@@ -0,0 +1,11 @@
+package io.github.jcrispo.auth;
+
+import org.springframework.boot.SpringApplication;
+
+public class AuthServiceApplication {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+        SpringApplication.run(AuthServiceApplication.class, args);
+    }
+}
